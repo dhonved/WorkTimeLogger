e05a5ceeafd3cfbf8aa7c9ed09879a86cd779842
dhonved <daniel.honved@gmail.com>
2018-09-22 11:57:39 +0200

    Version update

--- WorkTimeLogger/Properties/AssemblyInfo.cs
+++ WorkTimeLogger/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WorkTimeLogger")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helper application to keep track of time spent on various work items")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WorkTimeLogger")]
@@ -51,7 +51,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
